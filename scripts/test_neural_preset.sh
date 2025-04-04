4c65f68f6f8fc28bfc7da953f0a98a156bbefc25
DY112 <dongyoung.kim@yonsei.ac.kr>
2025-04-04 16:31:35 +0900

    update for test mode

--- scripts/test_neural_preset.sh
+++ scripts/test_neural_preset.sh
@@ -4,4 +4,4 @@ model.ver=v1 \
 model.solver=v1 \
 mode=test \
 data.name=test_data \
-load.ckpt_path=../ckpts/250331_083655_neural_styler_v1/best_val_losses_total_loss_epoch=0031.ckpt \
+load.ckpt_path=../ckpts/250331_083655_neural_styler_v1/best_val_losses_total_loss_epoch=0031.ckpt # path to the best model ckpt
